0854f014539054a1f9dfaead36e9fb3ee7c3f953
Ian <iank@netvirta.com>
2021-01-21 10:18:21 +0800

    Fix for compiling filament

--- tools/matc/src/matc/JsonishLexer.h
+++ tools/matc/src/matc/JsonishLexer.h
@@ -16,22 +16,26 @@
 
 #ifndef TNT_JSONLEXER_H
 #define TNT_JSONLEXER_H
+#include <string.h>
 
 #include "Lexer.h"
 #include "JsonishLexeme.h"
 
-namespace matc {
+namespace matc
+{
 
-// Breaks down a stream of chars into JsonLexemes.
-class JsonishLexer final: public Lexer<JsonLexeme> {
-public:
-    virtual bool readLexeme() noexcept;
-private:
-    JsonType readIdentifier() noexcept;
-    void readNumber() noexcept;
-    void readString() noexcept;
-    JsonType readPunctuation() noexcept;
-};
+    // Breaks down a stream of chars into JsonLexemes.
+    class JsonishLexer final : public Lexer<JsonLexeme>
+    {
+    public:
+        virtual bool readLexeme() noexcept;
+
+    private:
+        JsonType readIdentifier() noexcept;
+        void readNumber() noexcept;
+        void readString() noexcept;
+        JsonType readPunctuation() noexcept;
+    };
 
 } // namespace matc
 
